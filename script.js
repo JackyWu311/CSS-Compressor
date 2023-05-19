9e35623a6ddb20e5247c519de3681df6c5fdce57
JackyWu311 <apple970066@gmail.com>
2023-05-20 02:14:05 +0800

    adjust style

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ document.getElementById('compressBtn').addEventListener('click', function() {
   // 壓縮 CSS
   var compressedCSS = compressCSS(input);
   
+  // 更新輸出框
+  document.getElementById('output').value = compressedCSS;
+  
   // 計算原始大小和壓縮後大小
   var originalSize = input.length;
   var compressedSize = compressedCSS.length;
@@ -24,14 +27,24 @@ document.getElementById('compressBtn').addEventListener('click', function() {
   // 計算節省百分比
   var savingsPercentage = ((originalSize - compressedSize) / originalSize * 100).toFixed(2);
   
-  // 更新輸出框和相關資訊
-  document.getElementById('output').value = compressedCSS;
-  document.getElementById('originalSize').textContent = originalSize;
-  document.getElementById('compressedSize').textContent = compressedSize;
-  document.getElementById('savingsPercentage').textContent = savingsPercentage;
+  // 顯示原始大小、壓縮後大小和節省百分比資訊
+  var infoContainer = document.getElementById('infoContainer');
+  infoContainer.innerHTML = `
+    <p>原始大小: <span>${originalSize} bytes</span></p>
+    <p>壓縮後大小: <span>${compressedSize} bytes</span></p>
+    <p>節省了 <span>${savingsPercentage}%</span> 的大小</p>
+  `;
   
   // 顯示複製按鈕
   document.getElementById('copyBtn').style.display = 'inline';
 });
 
 // 監聽複製按鈕
+document.getElementById('copyBtn').addEventListener('click', function() {
+  // 將壓縮後的 CSS 內容複製到剪貼簿
+  var output = document.getElementById('output');
+  output.select();
+  document.execCommand('copy');
+  alert('已複製壓縮後的 CSS 內容到剪貼簿');
+  });
+  
